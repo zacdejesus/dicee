0c3707c439d5c5e71a25fb929bf0b114f676c7f9
Alejandro de jesus <z@zadkiels-macbook-air.local>
2019-03-15 12:56:39 -0300

    Initial Commit

---
+++ dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dicee
+//
+//  Created by zadkiel de jesus on 15/03/2019.
+//  Copyright © 2019 zadkiel de jesus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
